e91db1f63e510a0a20bbaa9cb2cff7a1cb637ec0
Kantrawibawa10 <githubsaya@gmail.com>
2023-12-26 12:22:17 +0800

    first add project

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,132 @@
+@extends('layouts.admin')
+@section('body')
+<style>
+    .warning{
+        background-color: rgb(255, 146, 29);
+    }
+</style>
+<div class="main-content">
+    <section classs="section">
+        <div class="row">
+            <div class="col-lg-4 col-md-4 col-sm-12">
+                <div class="card card-statistic-2">
+                    <div class="card-icon warning">
+                        <i class="fas fa-archive"></i>
+                    </div>
+                    <div class="card-wrap">
+                        <div class="card-header">
+                            <h4>Total Orders</h4>
+                        </div>
+                        <div class="card-body">
+                            59
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-4 col-sm-12">
+                <div class="card card-statistic-2">
+                    <div class="card-icon warning">
+                        <i class="fas fa-dollar-sign"></i>
+                    </div>
+                    <div class="card-wrap">
+                        <div class="card-header">
+                            <h4>Penghasilan</h4>
+                        </div>
+                        <div class="card-body">
+                            $187,13
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-lg-4 col-md-4 col-sm-12">
+                <div class="card card-statistic-2">
+                    <div class="card-icon warning">
+                        <i class="fas fa-shopping-bag"></i>
+                    </div>
+                    <div class="card-wrap">
+                        <div class="card-header">
+                            <h4>Penjualan</h4>
+                        </div>
+                        <div class="card-body">
+                            4,732
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+        <div class="row">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">
+                        <h4>Invoices</h4>
+                        <div class="card-header-action">
+                            <a href="#" class="btn btn-danger">View More <i class="fas fa-chevron-right"></i></a>
+                        </div>
+                    </div>
+                    <div class="card-body p-0">
+                        <div class="table-responsive table-invoice">
+                            <table class="table table-striped" style="font-size: 12px;">
+                                <tr>
+                                    <th>Invoice ID</th>
+                                    <th>Customer</th>
+                                    <th>Status</th>
+                                    <th>Due Date</th>
+                                    <th>Action</th>
+                                </tr>
+                                <tr>
+                                    <td><a href="#">INV-87239</a></td>
+                                    <td class="font-weight-600">Kusnadi</td>
+                                    <td>
+                                        <div class="badge badge-warning">Unpaid</div>
+                                    </td>
+                                    <td>July 19, 2018</td>
+                                    <td>
+                                        <a href="#" class="btn btn-warning">Detail</a>
+                                    </td>
+                                </tr>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-lg-4">
+                <div class="card gradient-bottom">
+                    <div class="card-header">
+                        <h4>Top 5 Products</h4>
+                    </div>
+                    <div class="card-body mb-3" id="top-5-scroll">
+                        <ul class="list-unstyled list-unstyled-border">
+                            <li class="media">
+                                <img class="mr-3 rounded" width="55" src="assets/img/products/product-3-50.png"
+                                    alt="product">
+                                <div class="media-body">
+                                    <div class="float-right">
+                                        <div class="font-weight-600 text-muted text-small">86 Sales</div>
+                                    </div>
+                                    <div class="media-title">oPhone S9 Limited</div>
+                                    <div class="mt-1">
+                                        <div class="budget-price">
+                                            <div class="budget-price-square bg-warning" data-width="64%">
+                                            </div>
+                                            <div class="budget-price-label">$68,714</div>
+                                        </div>
+                                        <div class="budget-price">
+                                            <div class="budget-price-square bg-danger" data-width="43%">
+                                            </div>
+                                            <div class="budget-price-label">$38,700</div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</div>
+@endsection
